aabc08e2b2c865981a0ffd83788c30d1db5cc872
jgi <public-devgit-common@gissehel.org>
2019-09-15 13:03:28 +0200

    Add deploy script to generate dist-deploy

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-import config from '../webpack.config';
+import webpackConfig from '../webpack.config';
 import express from 'express';
-import configureHot from './server/configure/hot';
-import configureProd from './server/configure/prod'
 import listenApplication from './server/listen';
 
 const app = express();
 
-if (config.mode === 'development') {
+if (webpackConfig.mode === 'development') {
+    const configure = require('./server/configure/hot').default;
     console.log('Application is in development mode');
-    configureHot(app, config);
+    configure(app, webpackConfig);
 } else {
+    const configure = require('./server/configure/prod').default;
     console.log('Application is in production mode');
-    configureProd(app, config);
+    configure(app, webpackConfig);
 }
 
 listenApplication(app);
