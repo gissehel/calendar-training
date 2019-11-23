6a23e5aaaa0acb2139b91d603f3f51aa41e57509
jgi <public-vs2015@gissehel.org>
2019-11-23 16:32:40 +0100

    FEATURE : Date in french

--- src/client/service/day.js
+++ src/client/service/day.js
@@ -4,7 +4,7 @@ const stop_date = new Date('2050-01-01');
 export const findNewDay = () => {
     const time = start_date.getTime()+(stop_date.getTime()-start_date.getTime())*Math.random();
     const date = new Date(time);
-    const dateStr = date.toLocaleDateString()
+    const dateStr = date.toLocaleDateString("fr")
     const expectedResult = date.getDay();
 
     return {time, date, dateStr, expectedResult};
